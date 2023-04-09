8da6cf5e2ecfc538d0e41cc3fc1ebebd54b8fabc
alpetrushkin <alpetrushkin1984@mail.ru>
2023-04-09 11:35:20 +0300

    отображение текста об отсутствии задач

--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Todo from "./Todo";
 import s from './TodoList.module.css';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({todos}) => {
+    let todosMap = todos.map((todo, index) => <Todo key={index} todo={todo}/>)
+
     return (
         <div className={s.todoListBox}>
-            {todos.map((todo, index) => <Todo key={index} todo={todo}/>)}
+            {!todos.length ? <h2>Todo list is empty</h2> : todosMap}
         </div>
-
     )
 };
 
